510b9b91704a65b7229e8b0fe4f7c11b0c6d9524
PopoiuAndra <126085931+PopoiuAndra@users.noreply.github.com>
2024-04-18 15:26:27 +0300

    Prima incercare

--- OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/QuestionAnswerPair.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/QuestionAnswerPair.cs
@@ -1,6 +1,27 @@
-﻿namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities
 {
     public class QuestionAnswerPair
     {
+        private Guid questionId{ get; set; }
+
+        private List<System.Object> answer { get; set; }
+
+        private QuizSubmission quizSubmission { get; set; }
+
+        public QuestionAnswerPair (Guid questionId, List<System.Object> answer, QuizSubmission quizSubmission)
+        {
+            this.questionId = questionId;
+            this.answer = answer;
+            this.quizSubmission = quizSubmission;
+        }      
+        
+        public QuestionAnswerPair (QuestionAnswerPair questionAnswerPair)
+        {
+            this.questionId = questionAnswerPair.questionId;    
+            this.answer = questionAnswerPair.answer;    
+            this.quizSubmission = questionAnswerPair.quizSubmission;    
+        }
     }
 }
--- OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/QuizSubmission.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/QuizSubmission.cs
@@ -2,5 +2,20 @@
 {
     public class QuizSubmission
     {
+        private Guid quizId { get; set; } 
+
+        private Guid takenBy { get; set; }
+
+        private List<QuestionAnswerPair> answers { get; set; } = new();
+
+        private int timeElapsed { get; set; } = 0;
+
+        public QuizSubmission(Guid quizId, Guid takenBy, List<QuestionAnswerPair> answers, int timeElapsed)
+        {
+            this.quizId = quizId;
+            this.takenBy = takenBy;
+            this.answers = answers;
+            this.timeElapsed = timeElapsed;
+        }
     }
 }
