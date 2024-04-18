cf3db657754ebd17c981c79bfd92076cc11ac877
emiliaaa25 <galatanuemilia14@gmail.com>
2024-04-18 13:49:36 +0200

    updated

--- OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/QuestionResult.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/QuestionResult.cs
@@ -2,15 +2,44 @@
 {
     public class QuestionResult
     {
+        private object questionFromDd;
+
         public Guid QuestionId { get; set; }
-        public List<Object> SubmittedAnswers { get; set; }= new List<Object>();
-        public List<AnswerResult> AnswerType { get; set; } = new List<AnswerResult>();
+        public List<Object> SubmittedAnswers { get; set; } = new();
+        public List<AnswerResult> AnswersType { get; set; } = new();
         public int Score { get; set; }
+
+        private QuestionResult(Guid questionId, List<object> submittedAnswers)
+        {
+            QuestionId = questionId;
+            SubmittedAnswers = submittedAnswers;
+        }
 
         public void UpdateScore(int finalScore)
         {
-            finalScore += Score;
+            Score = finalScore;
+            if (Score == 0)
+            {
+                AnswersType[0] = AnswerResult.Wrong;
+                return;
+            }
+            questionFromDd = GetQuestionFromDB(QuestionId);
+            if (Score == questionFromDd.AllocatedPoints)
+            {
+                AnswersType[0] = AnswerResult.Correct;
+                return;
+            }
+            else
+            {
+                AnswersType[0] = AnswerResult.PartiallyCorrect;
+                return;
+            }
+
         }
 
+        private QuestionResult GetQuestionFromDB(Guid questionId)
+        {
+            // return cevaaaaa;
+        }
     }
 }
--- OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/QuizResultBody.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/QuizResultBody.cs
@@ -1,13 +1,40 @@
-﻿namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities
+﻿using Microsoft.EntityFrameworkCore.Update.Internal;
+
+namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities
 {
     public class QuizResultBody
     {
+        private List<QuizResultHeader> QuizResultHeaders=new();
+
         public Guid QuizzId { get; set; }
         public Guid UserId { get; set; } 
-        private QuestionResult QuestionResult { get; set; } = new QuestionResult();
-        public void ReviewAnswer(int finalScore)
+        private List<QuestionResult> QuestionResults { get; set; } = new();
+        public void ReviewAnswer(Guid questionId, int finalScore)
         {
-            QuestionResult.UpdateScore(finalScore);   
+            var questionResult = QuestionResults.FirstOrDefault(q => q.QuestionId == questionId);
+            if (questionResult != null)
+            {
+                questionResult.UpdateScore(finalScore);
+            }
+            //UpdateInDB();
+
+            QuizResultHeader foundQHR = GetQuizResultHeader(QuizzId, UserId);
+            if (foundQHR != null)
+            {
+                foundQHR.UpdateUponAnswerReview(this);
+            }
+        }
+
+        public QuizResultBody(Guid quizzId, Guid userId)
+        {
+            QuizzId = quizzId;
+            UserId = userId;
+        }
+
+        private QuizResultHeader GetQuizResultHeader(Guid quizId, Guid userId)
+        {
+            return QuizResultHeaders.FirstOrDefault(q => q.quizID == QuizzId && q.userID == UserId);
         }
     }
+
 }
