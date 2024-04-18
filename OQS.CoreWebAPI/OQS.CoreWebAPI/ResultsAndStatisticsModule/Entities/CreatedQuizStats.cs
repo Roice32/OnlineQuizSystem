96c1ceeafca9ba187f65d124865d560f29eb160c
Casiana <88460309+casiana2075@users.noreply.github.com>
2024-04-19 00:40:01 +0300

    CreatedQuizStats & TakenQuizzesHistory classes

--- OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/CreatedQuizStats.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/CreatedQuizStats.cs
@@ -2,5 +2,22 @@
 {
     public class CreatedQuizStats
     {
+        public Guid UserID { get; set; }
+        public Guid QuizId { get; set; }
+        public List<QuizResultHeader> QuizResultHeaders { get; set; }
+
+        public CreatedQuizStats(Guid userID, Guid quizId)
+        {
+            UserID = userID;
+            QuizId = quizId;
+            QuizResultHeaders = FetchQuizResultHeaders(UserID, QuizId);
+        }
+
+        private List<QuizResultHeader> FetchQuizResultHeaders(Guid userID, Guid quizId)
+        {
+            // Database fetching logic here
+            // PLACEHOLDER
+            return new List<QuizResultHeader>();
+        }
     }
 }
--- OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/TakenQuizzesHistory.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/TakenQuizzesHistory.cs
@@ -2,5 +2,20 @@
 {
     public class TakenQuizzesHistory
     {
+        public Guid UserId { get; set; }
+        public List<QuizResultHeader> QuizResultHeaders { get; set; }
+
+        public TakenQuizzesHistory(Guid userId)
+        {
+            UserId = userId;
+            QuizResultHeaders = FetchQuizResultHeaders(UserId);
+        }
+
+        private List<QuizResultHeader> FetchQuizResultHeaders(Guid userId)
+        {
+            // Database fetching logic here
+            // PLACEHOLDER
+            return new List<QuizResultHeader>();
+        }
     }
 }
