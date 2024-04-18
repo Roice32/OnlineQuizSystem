2109e8ae01729e3962812ca9d1d9323b8a199786
Roice32 <alexandru.butnaru32@yahoo.com>
2024-04-18 17:52:38 +0300

    Added written classes back.

--- OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/QuizResultBody.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/QuizResultBody.cs
@@ -1,15 +1,40 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore.Update.Internal;
+
 namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities
 {
     public class QuizResultBody
     {
-        public Guid QuizzId { get; set; }
-        public Guid UserId { get; set; }
-        public IEnumerable<QuestionResult> QuestionResults { get; internal set; }
-        private QuestionResult QuestionResult { get; set; } = new QuestionResult();
-        public void ReviewAnswer(int finalScore)
+        private List<QuizResultHeader> QuizResultHeaders=new();
+
+        public Guid QuizId { get; set; }
+        public Guid UserId { get; set; } 
+        private List<QuestionResult> QuestionResults { get; set; } = new();
+        public void ReviewAnswer(Guid questionId, int finalScore)
         {
-            QuestionResult.UpdateScore(finalScore);
+            var questionResult = QuestionResults.FirstOrDefault(q => q.QuestionId == questionId);
+            if (questionResult != null)
+            {
+                questionResult.UpdateScore(finalScore);
+            }
+            //UpdateInDB();
+
+            QuizResultHeader foundQHR = GetQuizResultHeader(QuizId, UserId);
+            if (foundQHR != null)
+            {
+                foundQHR.UpdateUponAnswerReview(this);
+            }
+        }
+
+        public QuizResultBody(Guid quizzId, Guid userId)
+        {
+            QuizId = quizzId;
+            UserId = userId;
+        }
+
+        private QuizResultHeader GetQuizResultHeader(Guid QuizId, Guid UserId)
+        {
+            return QuizResultHeaders.FirstOrDefault(q => q.QuizId == QuizId && q.UserId == UserId);
         }
     }
+
 }
