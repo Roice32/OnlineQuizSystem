25aaa11d8eb54dc4ad553d77fc892bfd3aa743f4
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2024-04-20 22:35:15 +0300

    Add tailwind

---
+++ b4quiz/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const HomePage = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Bine ai venit pe pagina noastră!</h1>
+        </div>
+    );
+};
+
+export default HomePage;
