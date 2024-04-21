870652c3abe57849873bd56ddcfd645e71a6326f
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2024-04-21 14:37:31 +0300

    ActiveQuizCreate

--- OQS.CoreWebAPI/OQS.CoreWebAPI/obj/Debug/net8.0/OQS.CoreWebAPI.AssemblyInfo.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/obj/Debug/net8.0/OQS.CoreWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OQS.CoreWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538c97a137d63715cd0d35a33af07ed84fb462d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe007219c94d7e69c222d9dd5001760e128acde8")]
 [assembly: System.Reflection.AssemblyProductAttribute("OQS.CoreWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OQS.CoreWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
