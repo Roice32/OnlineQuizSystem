91f7998e56c3db95f5518870a900acdc67043353
alexfrunza <frunza.alexandru.2004@gmail.com>
2024-04-21 18:07:45 +0300

    Change attributes names

--- OQS.CoreWebAPI/OQS.CoreWebAPI/Entities/Questions/SingleChoiceQuestion.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/Entities/Questions/SingleChoiceQuestion.cs
@@ -1,11 +1,12 @@
 namespace OQS.CoreWebAPI.Entities;
 
-public class SingleChoiceQuestion:ChoiceQuestionBase
+public class SingleChoiceQuestion : ChoiceQuestionBase
 {
-    public string SingleChoiceAnswer { get;set; } = string.Empty;
-    public SingleChoiceQuestion(Guid id, string text, List<string> choices, string singleChoiceAnswer):base(id,text,QuestionType.SingleChoice,choices)
+    public string SingleChoiceAnswer { get; set; } = string.Empty;
+
+    public SingleChoiceQuestion(Guid id, string text, List<string> choices, string singleChoiceAnswer) : base(id, text,
+        QuestionType.SingleChoice, choices)
     {
         SingleChoiceAnswer = singleChoiceAnswer;
     }
-   
 }
