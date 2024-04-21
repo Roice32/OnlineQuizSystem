240befdc7dbd694969e6c47941f38adcb8da8a2c
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2024-04-21 13:16:00 +0300

    Remove App js file

--- b4quiz/src/App.tsx
+++ b4quiz/src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App(): JSX.Element {
-    return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    );
+import Navbar from './components/Navbar';
+
+function App() : JSX.Element {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
