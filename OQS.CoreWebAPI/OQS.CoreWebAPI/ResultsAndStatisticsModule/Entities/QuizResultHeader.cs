71511acdd3db84a70b3ca385ebdaf136f03db979
Roice32 <alexandru.butnaru32@yahoo.com>
2024-04-21 15:35:08 +0300

    Manually fetched correct project structure.

--- OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/QuizResultHeader.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/QuizResultHeader.cs
@@ -5,17 +5,17 @@ namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities
 {
     public class QuizResultHeader
     {
-        public Guid QuizId { get; }
-        public Guid UserId { get; }
+        public Guid QuizId { get; set; }
+        public Guid UserId { get; set;  }
         public DateTime SubmittedAt { get; set; }
-        public int CompletionTime { get; }
+        public int CompletionTime { get; set; }
         public float Score { get; set; }
         public bool ReviewPending { get; set; }
 
-        public QuizResultHeader(Guid quizID, Guid userID, int completionTime)
+        public QuizResultHeader(Guid quizId, Guid userId, int completionTime)
         {
-            QuizId = quizID;
-            UserId = userID;
+            QuizId = quizId;
+            UserId = userId;
             CompletionTime = completionTime;
             SubmittedAt = DateTime.Now;
         }
