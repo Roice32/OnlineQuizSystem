a7f5cea2f4b175089b351fc4f8b82cfa46770bd2
Alexandru-Vasilica <alexandru.vasilica97@gmail.com>
2024-04-21 21:49:58 +0300

    Changed seed

--- OQS.CoreWebAPI/OQS.CoreWebAPI/Extensions/SeedActiveQuizzesExtension.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/Extensions/SeedActiveQuizzesExtension.cs
@@ -7,16 +7,15 @@ public static class SeedActiveQuizzesExtension
 {
     public static void SeedActiveQuizzes(this ApplicationDBContext context)
     {
-        if (context.ActiveQuizzes.Any())
+        if (context.ActiveQuizzes.Any(quiz=>quiz.Id == Guid.Parse("f0a486df-a7bd-467f-bb9a-4ac656972450")))
         {
             return;
         }
-
         var quiz = context.Quizzes.First(q => q.Id == Guid.Parse("1af3912f-d625-413a-91b6-cb31f4cbb13b"));
         var user = context.Users.First(u => u.Id == Guid.Parse("5b048913-5df0-429f-a42b-051904672e4d"));
         var ActiveQuiz = new ActiveQuiz()
         {
-            Id = Guid.NewGuid(),
+            Id = Guid.Parse("f0a486df-a7bd-467f-bb9a-4ac656972450"),
             Quiz = quiz,
             User = user,
         };
