87be44d6de4f58f7c9fb57223a1d1c4e107ff642
PopoiuAndra <126085931+PopoiuAndra@users.noreply.github.com>
2024-04-22 20:53:17 +0300

    GetQuizResultHeaderResponse

---
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Contracts/GetQuizResultHeaderResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Contracts
+{
+    public class GetQuizResultHeaderResponse
+    {
+        public Guid QuizId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime SubmittedAt { get; set; }
+        public int CompletionTime { get; set; }
+        public float Score { get; set; }
+        public bool ReviewPending { get; set; }
+        // PLACEHOLDER
+        string UserName { get; set; }
+        // PLACEHOLDER
+        string QuizName { get; set; }
+    }
+}
