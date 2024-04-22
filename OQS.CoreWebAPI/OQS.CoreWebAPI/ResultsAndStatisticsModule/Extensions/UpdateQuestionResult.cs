3d345d82500b7bd1a06f587bd0272c91760cfcee
denisagolache <126023804+denisagolache@users.noreply.github.com>
2024-04-22 18:33:24 +0200

    github ........

--- OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Extensions/UpdateQuestionResult.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Extensions/UpdateQuestionResult.cs
@@ -1,6 +1,16 @@
-﻿namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Extensions
+﻿using OQS.CoreWebAPI.ResultsAndStatisticsModule.Database;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities.QuestionResults;
+
+namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Extensions
 {
-    public class UpdateQuestionResult
+    public static class UpdateQuestionResult
     {
+        public static void UpdateResult(this WebApplication application, QuestionResultBase questionResult)
+        {
+            using var scope = application.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<RSMApplicationDbContext>();
+            context.QuestionResults.Update(questionResult);
+            context.SaveChanges();
+        }                                                                                                   
     }
 }
