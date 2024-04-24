762bebe56397cad0cabba489888d2247a67673c1
emiliaaa25 <galatanuemilia14@gmail.com>
2024-04-24 11:55:06 +0200

    .

---
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Contracts/ReviewAnswerResponse.cs
@@ -0,0 +1,11 @@
+﻿using OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities.QuestionResults;
+
+namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Contracts
+{
+    public class ReviewAnswerResponse
+    {
+        public QuizResultHeader UpdatedQuizResultHeader { get; set; }
+        public QuestionResultBase UpdatedQuestionResult { get; set; }
+    }
+}
---
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Extensions/QuizResultHeaders/UpdateHeaderUponAnswerReviewExtension.cs
@@ -0,0 +1,50 @@
+﻿using OQS.CoreWebAPI.ResultsAndStatisticsModule.Database;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities.QuestionResults;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Temp;
+
+namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Extensions.QuizResultHeaders
+{
+    public static class UpdateHeaderUponAnswerReviewExtension
+    {
+        public static void UpdateHeaderUponAnswerReview(this WebApplication application, Guid userId, Guid quizId)
+        {
+            using var scope = application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<RSMApplicationDbContext>();
+            UpdateHeaderUponAnswerReview(dbContext, userId, quizId);
+        }
+
+        public static void UpdateHeaderUponAnswerReview(RSMApplicationDbContext dbContext, Guid userId, Guid quizId)
+        {
+            var quizResultHeader = dbContext.QuizResultHeaders
+                .FirstOrDefault(qrh => qrh.UserId == userId && qrh.QuizId == quizId);
+            var questionIds = dbContext.QuizResultBodies
+                .Where(qrb => qrb.UserId == userId && qrb.QuizId == quizId)
+                .Select(qrb => qrb.QuestionIds)
+                .FirstOrDefault();
+            var questionResults = dbContext.QuestionResults
+                .Where(qr => questionIds.Contains(qr.QuestionId) && qr.UserId == userId)
+                .ToList();
+
+            // PLACEHOLDER
+            // Only for testing API till we get quizzes database.
+            if(quizResultHeader is null || questionIds is null || questionResults is null)
+            {
+                return;
+            }
+
+            quizResultHeader.Score = 0;
+            quizResultHeader.ReviewPending = false;
+            foreach (var qaPair in questionResults)
+            {
+                quizResultHeader.Score += qaPair.Score;
+                if (qaPair is ReviewNeededQuestionResult &&
+                                       ((ReviewNeededQuestionResult)qaPair).ReviewNeededResult == AnswerResult.Pending)
+                    quizResultHeader.ReviewPending = true;
+            }
+
+            dbContext.QuizResultHeaders.Update(quizResultHeader);
+            dbContext.SaveChanges();
+        }
+    }
+}
