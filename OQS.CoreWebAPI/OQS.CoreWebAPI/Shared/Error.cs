6948fb2b27d8c61b49834d65ac6435a016ac91fc
denisagolache <126023804+denisagolache@users.noreply.github.com>
2024-05-08 18:20:35 +0200

    .

--- OQS.CoreWebAPI/OQS.CoreWebAPI/Shared/Error.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OQS.CoreWebAPI.Shared
-{
-    public record Error(string Code, string Message)
-    {
-        public static readonly Error None = new(string.Empty, string.Empty);
-        public static readonly Error NullValue = new("Error.NullValue", "Value cannot be null.");
-        public static readonly Error ConditionNotMet = new("Error.ConditionNotMet", "Condition not met.");
-    }
-}
