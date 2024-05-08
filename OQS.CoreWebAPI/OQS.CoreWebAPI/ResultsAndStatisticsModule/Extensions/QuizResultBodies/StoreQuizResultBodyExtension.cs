10eefdad4b98912afd7a18e382e1f1a5f63c66ae
emiliaaa25 <galatanuemilia14@gmail.com>
2024-05-08 18:34:36 +0200

    .

---
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Extensions/QuizResultBodies/StoreQuizResultBodyExtension.cs
@@ -0,0 +1,16 @@
+﻿using OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities;
+using OQS.CoreWebAPI.Database;
+
+namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Extensions
+{
+    public static class StoreQuizResultBodyExtension
+    {
+        public static async Task StoreQuizResultBodyAsync(this WebApplication application, QuizResultBody quizResultBody)
+        {
+            using var scope = application.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            await context.QuizResultBodies.AddAsync(quizResultBody);
+        }
+    }
+}
