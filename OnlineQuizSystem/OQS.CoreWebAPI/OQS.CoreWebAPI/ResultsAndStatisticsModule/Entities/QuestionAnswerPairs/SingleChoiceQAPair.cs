f615771be872bcbeba7a8f700e5778a377a5e343
denisagolache <126023804+denisagolache@users.noreply.github.com>
2024-05-08 20:40:00 +0200

    new project structure

---
+++ OnlineQuizSystem/OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Entities/QuestionAnswerPairs/SingleChoiceQAPair.cs
@@ -0,0 +1,12 @@
+﻿namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities.QuestionAnswerPairs
+{
+    public class SingleChoiceQAPair : QuestionAnswerPairBase
+    {
+        public string SingleChoiceAnswer { get; set; }
+
+        public SingleChoiceQAPair(Guid questionId, string answer) : base(questionId)
+        {
+            SingleChoiceAnswer = answer;
+        }
+    }
+}
