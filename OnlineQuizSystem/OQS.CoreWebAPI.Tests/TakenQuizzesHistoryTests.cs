ec58a42e2040744c8f4161d57d5620e6a2dfa22f
PopoiuAndra <126085931+PopoiuAndra@users.noreply.github.com>
2024-05-12 00:31:09 +0300

    takenQuizzesHistory

--- OnlineQuizSystem/OQS.CoreWebAPI.Tests/TakenQuizzesHistoryTests.cs
+++ OnlineQuizSystem/OQS.CoreWebAPI.Tests/TakenQuizzesHistoryTests.cs
@@ -1,4 +1,6 @@
 ﻿using FluentAssertions;
+using Newtonsoft.Json;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Contracts;
 using OQS.CoreWebAPI.ResultsAndStatisticsModule.Features;
 using OQS.CoreWebAPI.ResultsAndStatisticsModule.Temp;
 using OQS.CoreWebAPI.Tests.SetUp;
@@ -17,7 +19,7 @@ namespace OQS.CoreWebAPI.Tests
         public async Task Given_IdForNonexistentUser_When_GetTakenQuizzesHistory_Then_NullValueIsReturned()
         {
             // Arrange
-            var userId = Guid.Parse("00000000-0000-0000-0001-000000000007");
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000009");
             var requestUri = "api/quizResults/getTakenQuizzesHistory/" + userId;
 
             // Act
@@ -26,10 +28,47 @@ namespace OQS.CoreWebAPI.Tests
             // Assert
             result.StatusCode.Should().Be(HttpStatusCode.NotFound);
         }
+        
+        [Fact]
+        public async Task When_GetTakenQuizzesHistoryIsCalled_Then_CorrectStatsAreReturned()
+        {
+            // Arrange
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000001");
+            var requestUri = "api/quizResults/getTakenQuizzesHistory/" + userId;
+
+            // Act
+            var result = await Client.GetAsync(requestUri);
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            var resultString = await result.Content.ReadAsStringAsync();
+            var resultObject = JsonConvert.DeserializeObject<GetTakenQuizzesHistoryResponse>(resultString);
+
+            resultObject.QuizNames.Should().HaveCount(1);
+            resultObject.QuizResultHeaders.Should().HaveCount(1);
+            resultObject.QuizNames.Should().ContainKey(Guid.Parse("00000000-0000-0000-0002-000000000001"));
+            resultObject.QuizNames[Guid.Parse("00000000-0000-0000-0002-000000000001")].Should().Be("Quiz1");
+            resultObject.QuizResultHeaders[0].QuizId.Should().Be(Guid.Parse("00000000-0000-0000-0002-000000000001"));
+
+        }
 
         [Fact]
-        public async Task Task2()
+        public async Task Give_UserDidNotTakeAnyQuizzes_When_GetCreatedQuizStatsHandlerIsCalled_Then_EmptyQuizNamesAndQuizResultHeadersAreReturned()
         {
+            // Arrange
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000002");
+            var requestUri = "api/quizResults/getTakenQuizzesHistory/" + userId;
+
+            // Act
+            var result = await Client.GetAsync(requestUri);
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            var resultString = await result.Content.ReadAsStringAsync();
+            var resultObject = JsonConvert.DeserializeObject<GetTakenQuizzesHistoryResponse>(resultString);
+
+            resultObject.QuizNames.Should().BeEmpty();
+            resultObject.QuizResultHeaders.Should().BeEmpty();
 
         }
     }
