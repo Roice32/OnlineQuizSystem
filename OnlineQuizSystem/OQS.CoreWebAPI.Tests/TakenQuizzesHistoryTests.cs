1ec4c63908acb2056407c7d5c16a200c9480518d
PopoiuAndra <126085931+PopoiuAndra@users.noreply.github.com>
2024-05-11 19:58:32 +0300

    Update TakenQuizzesHistoryTests.cs

--- OnlineQuizSystem/OQS.CoreWebAPI.Tests/TakenQuizzesHistoryTests.cs
+++ OnlineQuizSystem/OQS.CoreWebAPI.Tests/TakenQuizzesHistoryTests.cs
@@ -1,9 +1,11 @@
-﻿using OQS.CoreWebAPI.ResultsAndStatisticsModule.Features;
+﻿using FluentAssertions;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Features;
 using OQS.CoreWebAPI.ResultsAndStatisticsModule.Temp;
 using OQS.CoreWebAPI.Tests.SetUp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,24 @@ namespace OQS.CoreWebAPI.Tests
 {
     public class TakenQuizzesHistoryTests : ApplicationContextForTesting
     {
-      
+        [Fact]
+        public async Task Given_IdForNonexistentUser_When_GetTakenQuizzesHistory_Then_NullValueIsReturned()
+        {
+            // Arrange
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000007");
+            var requestUri = "api/quizResults/getTakenQuizzesHistory/" + userId;
+
+            // Act
+            var result = await Client.GetAsync(requestUri);
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Task2()
+        {
+
+        }
     }
 }
