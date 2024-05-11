a46f14e9a8ce3859746305f4fee5f9c260d3a79e
PopoiuAndra <126085931+PopoiuAndra@users.noreply.github.com>
2024-05-11 04:05:31 +0300

    g

---
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/OQS.CoreWebAPI.Tests/QuestionsResultTests.cs
@@ -0,0 +1,194 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using OQS.CoreWebAPI.Database;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities.QuestionResults;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Extensions.QuestionResults;
+using OQS.CoreWebAPI.Shared;
+using OQS.CoreWebAPI.Tests.SetUp;
+
+namespace OQS.CoreWebAPI.Tests
+{
+    public class QuestionResultTests: ApplicationContextForTesting
+    {
+        [Fact]
+        public async Task Given_ValidIdsPair_When_FetchQuestionResultIsCalled_Then_CorrespondingResultIsReturnedAsync()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000001");
+            var questionId = Guid.Parse("00000000-0000-0000-0003-000000000005");
+
+            // Act
+            var result = await FetchQuestionResultExtension.FetchQuestionResultAsync(dbContext, userId, questionId);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ReviewNeededQuestionResult>();
+            
+            var reviewNeededResult = result as ReviewNeededQuestionResult;
+            reviewNeededResult.UserId.Should().Be(userId);
+            reviewNeededResult.QuestionId.Should().Be(questionId);
+        }
+
+        [Fact]
+        public void Given_IdsPairForNonexistentResult_When_FetchQuestionResultIsCalled_Then_NullIsReturned()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000001");
+            var questionId = Guid.Parse("00000000-0000-0000-0003-000000000006");
+
+            // Act
+            var result = FetchQuestionResultExtension.FetchQuestionResultAsync(dbContext, userId, questionId).Result;
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void When_FetchQuestionResultIsCalled_Then_AllQuestionResultTypesCanBeReturned()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000001");
+            var questionId1 = Guid.Parse("00000000-0000-0000-0003-000000000001");
+            var questionId2 = Guid.Parse("00000000-0000-0000-0003-000000000002");
+            var questionId3 = Guid.Parse("00000000-0000-0000-0003-000000000003");
+            var questionId4 = Guid.Parse("00000000-0000-0000-0003-000000000004");
+            var questionId5 = Guid.Parse("00000000-0000-0000-0003-000000000005");
+
+            // Act & Assert
+            var result1 = FetchQuestionResultExtension.FetchQuestionResultAsync(dbContext, userId, questionId1).Result;
+            result1.Should().NotBeNull();
+            result1.Should().BeOfType<TrueFalseQuestionResult>();
+            ((TrueFalseQuestionResult)result1).TrueFalseAnswerResult.Should().Be(AnswerResult.Correct);
+
+            var result2 = FetchQuestionResultExtension.FetchQuestionResultAsync(dbContext, userId, questionId2).Result;
+            result1.Should().NotBeNull();
+            result2.Should().BeOfType<ChoiceQuestionResult>();
+            var deserializedDictionary1 =
+                JsonConvert.DeserializeObject<Dictionary<string, AnswerResult>>(
+                    ((ChoiceQuestionResult)result2).PseudoDictionaryChoicesResults);
+            deserializedDictionary1.Should()
+                .BeEquivalentTo(new Dictionary<string, AnswerResult>
+                {
+                    {"Option1", AnswerResult.Other},
+                    {"Option2", AnswerResult.Correct},
+                    {"Option3", AnswerResult.Other}
+                });
+            
+            var result3 = FetchQuestionResultExtension.FetchQuestionResultAsync(dbContext, userId, questionId3).Result;
+            result1.Should().NotBeNull();
+            result3.Should().BeOfType<ChoiceQuestionResult>();
+            var deserializedDictionary2 = 
+                JsonConvert.DeserializeObject<Dictionary<string, AnswerResult>>(
+                    ((ChoiceQuestionResult)result3).PseudoDictionaryChoicesResults);
+            deserializedDictionary2.Should()
+                .BeEquivalentTo(new Dictionary<string, AnswerResult>
+                {
+                    { "Option1", AnswerResult.Wrong },
+                    { "Option2", AnswerResult.Correct },
+                    { "Option3", AnswerResult.Other },
+                    { "Option4", AnswerResult.CorrectNotPicked }
+                });
+
+            var result4 = FetchQuestionResultExtension.FetchQuestionResultAsync(dbContext, userId, questionId4).Result;
+            result4.Should().NotBeNull();
+            result4.Should().BeOfType<WrittenAnswerQuestionResult>();
+            ((WrittenAnswerQuestionResult)result4).WrittenAnswerResult.Should().Be(AnswerResult.Correct);
+            
+            var result5 = FetchQuestionResultExtension.FetchQuestionResultAsync(dbContext, userId, questionId5).Result;
+            result5.Should().NotBeNull();
+            result5.Should().BeOfType<ReviewNeededQuestionResult>();
+            ((ReviewNeededQuestionResult)result5).ReviewNeededResult.Should().Be(AnswerResult.PartiallyCorrect);
+        }
+
+        [Fact]
+        public void Given_IdsPairForNonexistentResult_When_UpdateQuestionResultIsCalled_Then_NullValueErrorIsReturned()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var questionId1 = Guid.NewGuid();
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000001");
+
+            // Act
+            var result = UpdateQuestionResultExtension.UpdateQuestionResultAsync(dbContext, userId, questionId1, 0).Result;
+            result.Should().BeEquivalentTo(Result.Failure(Error.NullValue));
+        }
+
+        [Fact]
+        public void Given_IdsPairForNonReviewNeededResult_When_UpdateQuestionResultIsCalled_Then_InvalidTypeErrorIsReturned()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var questionId1 = Guid.Parse("00000000-0000-0000-0003-000000000001");
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000001");
+
+            // Act
+            var result = UpdateQuestionResultExtension.UpdateQuestionResultAsync(dbContext, userId, questionId1, 0).Result;
+            result.Should().BeEquivalentTo(Result.Failure(Error.InvalidType));
+        }
+
+        [Fact]
+        public void Given_InvalidScore_When_UpdateQuestionResultIsCalled_Then_OutOfBoundsValueErrorIsReturned()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000001");
+            var questionId = Guid.Parse("00000000-0000-0000-0003-000000000005");
+
+            // Act & Assert
+            var result1 = UpdateQuestionResultExtension.UpdateQuestionResultAsync(dbContext, userId, questionId, 6.01f).Result;
+            result1.Should().BeEquivalentTo(Result.Failure(Error.OutOfBoundsValue));
+
+            var result2 = UpdateQuestionResultExtension.UpdateQuestionResultAsync(dbContext, userId, questionId, -1).Result;
+            result2.Should().BeEquivalentTo(Result.Failure(Error.OutOfBoundsValue));
+        }
+
+        [Fact]
+        public void Given_ValidParameters_When_UpdateQuestionResultIsCalled_Then_ResultIsUpdatedInDatabase()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000001");
+            var questionId = Guid.Parse("00000000-0000-0000-0003-000000000005");
+
+            // Act & Assert
+            var result1 = UpdateQuestionResultExtension.UpdateQuestionResultAsync(dbContext, userId, questionId, 0).Result;
+            result1.Should().BeEquivalentTo(Result.Success());
+            var updatedResult1 = FetchQuestionResultExtension.FetchQuestionResultAsync(dbContext, userId, questionId).Result;
+            updatedResult1.Should().NotBeNull();
+            updatedResult1.Should().BeOfType<ReviewNeededQuestionResult>();
+            ((ReviewNeededQuestionResult)updatedResult1).ReviewNeededResult.Should().Be(AnswerResult.Wrong);
+
+            var result2 = UpdateQuestionResultExtension.UpdateQuestionResultAsync(dbContext, userId, questionId, 1).Result;
+            result2.Should().BeEquivalentTo(Result.Success());
+            var updatedResult2 = FetchQuestionResultExtension.FetchQuestionResultAsync(dbContext, userId, questionId).Result;
+            updatedResult2.Should().NotBeNull();
+            updatedResult2.Should().BeOfType<ReviewNeededQuestionResult>();
+            ((ReviewNeededQuestionResult)updatedResult2).ReviewNeededResult.Should().Be(AnswerResult.PartiallyCorrect);
+
+            var result3 = UpdateQuestionResultExtension.UpdateQuestionResultAsync(dbContext, userId, questionId, 6).Result;
+            result3.Should().BeEquivalentTo(Result.Success());
+            var updatedResult3 = FetchQuestionResultExtension.FetchQuestionResultAsync(dbContext, userId, questionId).Result;
+            updatedResult3.Should().NotBeNull();
+            updatedResult3.Should().BeOfType<ReviewNeededQuestionResult>();
+            ((ReviewNeededQuestionResult)updatedResult3).ReviewNeededResult.Should().Be(AnswerResult.Correct);
+        }
+    }
+}
---
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/OQS.CoreWebAPI.Tests/QuizResultHeaderTests.cs
@@ -0,0 +1,158 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OQS.CoreWebAPI.Database;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Contracts;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Extensions;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Extensions.QuizResultHeaders;
+using OQS.CoreWebAPI.Shared;
+using OQS.CoreWebAPI.Tests.SetUp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OQS.CoreWebAPI.Tests
+{
+    public class QuizResultHeaderTests : ApplicationContextForTesting
+    {
+        [Fact]
+        public async Task When_QuizResultHeader_And_QuizName_And_UserName_Is_Not_Null_Should_Return_FetchQuizResultHeaderResponse()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000001");
+            var quizId = Guid.Parse("00000000-0000-0000-0002-000000000001");
+
+            // Act
+            var result = await FetchQuizResultHeaderExtension.FetchQuizResultHeaderAsync(dbContext, quizId, userId);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<Result<FetchQuizResultHeaderResponse>>();
+
+            var response = result.Value;
+            response.QuizId.Should().Be(quizId);
+            response.UserId.Should().Be(userId);
+        }
+
+        [Fact]
+        public void When_QuizResultHeader_Is_Null_Should_Return_Empty_Response()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000001");
+            var quizId = Guid.Parse("00000000-0000-0000-0002-000000000002");
+
+            // Act
+            var result = FetchQuizResultHeaderExtension.FetchQuizResultHeaderAsync(dbContext, quizId, userId).Result;
+
+            // Assert
+            result.Should().BeEquivalentTo(Result.Failure(Error.NullValue));
+        }
+
+        [Fact]
+        public void When_QuizName_Is_Null_Should_Return_Empty_Response()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000003");
+            var quizId = Guid.Parse("00000000-0000-0000-0002-000000000004");
+
+            // Act
+            var result = FetchQuizResultHeaderExtension.FetchQuizResultHeaderAsync(dbContext, quizId, userId).Result;
+
+            // Assert
+            result.Should().BeEquivalentTo(Result.Failure(Error.NullValue));
+        }
+
+        [Fact]
+        public void When_UserName_Is_Null_Should_Return_Empty_Response()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000004");
+            var quizId = Guid.Parse("00000000-0000-0000-0002-000000000003");
+
+            // Act
+            var result = FetchQuizResultHeaderExtension.FetchQuizResultHeaderAsync(dbContext, quizId, userId).Result;
+
+            // Assert
+            result.Should().BeEquivalentTo(Result.Failure(Error.NullValue));
+        }
+
+        [Fact]
+        public void When_QuizResultHeader_Is_Null_Should_Return_Failure()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000001");
+            var quizId = Guid.Parse("00000000-0000-0000-0002-000000000002");
+
+            // Act
+            var result = UpdateHeaderUponAnswerReviewExtension.UpdateHeaderUponAnswerReviewAsync(dbContext, quizId, userId).Result;
+            result.Should().BeEquivalentTo(Result.Failure(Error.NullValue));
+        }
+
+        [Fact]
+        public void When_QuestionIds_Is_Null_Should_Return_Failure()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000004");
+            var quizId = Guid.Parse("00000000-0000-0000-0002-000000000003");
+
+            // Act
+            var result = UpdateHeaderUponAnswerReviewExtension.UpdateHeaderUponAnswerReviewAsync(dbContext, quizId, userId).Result;
+            result.Should().BeEquivalentTo(Result.Failure(Error.NullValue));
+        }
+
+      /*  [Fact]
+        public void When_QaPair_Is_ReviewNeededQuestionResult_And_ReviewNeededResult_Is_Pending_Should_Return_Success()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000003");
+            var quizId = Guid.Parse("00000000-0000-0000-0002-000000000003");
+
+            // Act
+            var result = UpdateHeaderUponAnswerReviewExtension.UpdateHeaderUponAnswerReviewAsync(dbContext, quizId, userId).Result;
+            result.Should().BeEquivalentTo(Result.Success());
+            var updatedResult = FetchQuizResultBodyExtension.FetchQuizResultBodyAsync(dbContext, quizId, userId).Result.Value;
+            updatedResult.Should().NotBeNull();
+            updatedResult.ReviewPending.Should().BeTrue();
+        }
+
+        [Fact]
+        public void When_QaPair_Is_Not_ReviewNeededQuestionResult_Should_Return_None()
+        {
+            // Arrange
+            using var scope = Application.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var userId = Guid.Parse("00000000-0000-0000-0001-000000000001");
+            var quizId = Guid.Parse("00000000-0000-0000-0002-000000000001");
+
+            // Act
+            var result = UpdateHeaderUponAnswerReviewExtension.UpdateHeaderUponAnswerReviewAsync(dbContext, quizId, userId).Result;
+            result.Should().BeEquivalentTo(Result.Failure(Error.None));
+        }
+        */
+    }
+}
