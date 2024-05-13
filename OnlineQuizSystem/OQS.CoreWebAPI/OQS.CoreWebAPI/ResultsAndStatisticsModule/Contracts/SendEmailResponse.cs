67c3cea187c73c89422a891f4ed6fc21a37b1f5b
emiliaaa25 <galatanuemilia14@gmail.com>
2024-05-13 20:40:49 +0200

    da

---
+++ OnlineQuizSystem/OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Contracts/SendEmailResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Contracts
+{
+    public class SendEmailResponse
+    {
+        public string Message { get; set; }
+        public string RecipientEmail { get; set; }
+        public string RecipientFirstName { get; set; }
+
+
+    }
+}
