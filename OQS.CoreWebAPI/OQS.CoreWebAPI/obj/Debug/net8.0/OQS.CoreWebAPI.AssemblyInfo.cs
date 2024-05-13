0a576713271cc100a1a5e57f6a9712b4ff6e0a16
Pirlac Andrea-Diana <125950320+pirlacdianaA5@users.noreply.github.com>
2024-05-13 22:41:49 +0300

    update3

--- OQS.CoreWebAPI/OQS.CoreWebAPI/obj/Debug/net8.0/OQS.CoreWebAPI.AssemblyInfo.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/obj/Debug/net8.0/OQS.CoreWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OQS.CoreWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8310dd72664c879647613b1292ee5707ede9f1d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d63d31f2249c0ca4b6c66aad920211c7288cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("OQS.CoreWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OQS.CoreWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
