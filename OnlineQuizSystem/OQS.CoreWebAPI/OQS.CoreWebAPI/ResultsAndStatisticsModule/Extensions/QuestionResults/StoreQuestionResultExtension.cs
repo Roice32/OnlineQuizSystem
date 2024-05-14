3b76fd9e0693e1f50574969dfe170a4df2439a57
emiliaaa25 <galatanuemilia14@gmail.com>
2024-05-14 10:49:28 +0200

    luat de pe team 4

---
+++ OnlineQuizSystem/OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Extensions/QuestionResults/StoreQuestionResultExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OQS.CoreWebAPI.Database;
+using OQS.CoreWebAPI.ResultsAndStatisticsModule.Entities.QuestionResults;
+
+namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Extensions.QuestionResults
+{
+    public static class StoreQuestionResultExtension
+    {
+        public static async Task StoreQuestionResultAsync(this WebApplication application, QuestionResultBase questionResult)
+        {
+            using var scope = application.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            await StoreQuestionResultAsync(context, questionResult);
+        }
+
+        public static async Task StoreQuestionResultAsync(ApplicationDbContext dbContext, QuestionResultBase questionResult)
+        {
+            int count = await dbContext.QuestionResults.CountAsync();
+            await dbContext.QuestionResults.AddAsync(questionResult);
+            await dbContext.SaveChangesAsync();
+            int newCount = await dbContext.QuestionResults.CountAsync();
+        }
+    }
+}
