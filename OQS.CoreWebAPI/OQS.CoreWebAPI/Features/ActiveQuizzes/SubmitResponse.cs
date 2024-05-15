35a31d1621d8231fbe288fadba5bd1b97a93fdce
Alexandru-Vasilica <alexandru.vasilica97@gmail.com>
2024-05-15 17:00:22 +0300

    added deadline validation

--- OQS.CoreWebAPI/OQS.CoreWebAPI/Features/ActiveQuizzes/SubmitResponse.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/Features/ActiveQuizzes/SubmitResponse.cs
@@ -13,6 +13,7 @@ namespace OQS.CoreWebAPI.Features.Quizzes;
 public class SubmitResponseRequestValidator : AbstractValidator<SubmitResponseRequest>
     {
         private readonly IServiceScopeFactory _serviceScopeFactory;
+        private static int _latencySafeguardInSeconds = 5;
 
         public SubmitResponseRequestValidator(IServiceScopeFactory serviceScopeFactory)
         {
@@ -24,14 +25,37 @@ public class SubmitResponseRequestValidator : AbstractValidator<SubmitResponseRe
             RuleFor(x => x.ActiveQuizId)
                 .NotEmpty().WithMessage("Active Quiz ID is required.");
 
-            RuleFor(x => x.Answers)
-                .NotEmpty().WithMessage("Answers are required.");
-
             RuleFor(x => x)
                 .MustAsync(UserMatchesQuizAsync)
                 .WithMessage("Unauthorized. User does not match the one who started the quiz.");
+            RuleFor(x => x)
+                .MustAsync(ResponseRespectsDeadline)
+                .WithMessage("Submissions are closed.");
         }
+        private async Task<bool> ResponseRespectsDeadline(SubmitResponseRequest request,CancellationToken cancellationToken)
+        {
+            if (request == null || request.ActiveQuizId == null)
+            {
+                return false;
+            }
 
+            using var scope = _serviceScopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDBContext>();
+            var activeQuiz = await dbContext.ActiveQuizzes
+                .Include(aq => aq.Quiz)
+                .FirstOrDefaultAsync(aq => aq.Id == request.ActiveQuizId, cancellationToken);
+
+            if (activeQuiz == null )
+            {
+                return false;
+            }
+            if(DateTime.UtcNow>activeQuiz.StartedAt.AddMinutes(activeQuiz.Quiz.TimeLimitMinutes).AddSeconds(_latencySafeguardInSeconds))
+            {
+                return false;
+            }
+            return true;
+        }
+    
         private async Task<bool> UserMatchesQuizAsync(SubmitResponseRequest request, CancellationToken cancellationToken)
         {
             if (request == null || request.ActiveQuizId == null)
