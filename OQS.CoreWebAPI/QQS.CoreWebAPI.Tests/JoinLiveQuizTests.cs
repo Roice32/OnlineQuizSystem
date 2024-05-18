93f3d97310c2ca80279a6c2016c5c97b1aa95a93
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2024-05-18 17:40:53 +0300

    ActiveQuizCreate1

--- OQS.CoreWebAPI/QQS.CoreWebAPI.Tests/JoinLiveQuizTests.cs
+++ OQS.CoreWebAPI/QQS.CoreWebAPI.Tests/JoinLiveQuizTests.cs
@@ -1,17 +1,26 @@
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.SignalR;
-    using Microsoft.AspNetCore.SignalR.Client;
-    using Microsoft.AspNetCore.TestHost;
-    using Microsoft.Extensions.DependencyInjection;
-    using OQS.CoreWebAPI.Entities;
-    using OQS.CoreWebAPI.Features.WebSocket;
-    using OQS.CoreWebAPI.Tests.SetUp;
-    using Xunit;
 
-    namespace QQS.CoreWebAPI.Tests;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using OQS.CoreWebAPI.Database;
+using OQS.CoreWebAPI.Entities;
+using OQS.CoreWebAPI.Features.WebSocket;
+using OQS.CoreWebAPI.Tests.SetUp;
+using Xunit;
 
+namespace QQS.CoreWebAPI.Tests
+{
     public class JoinLiveQuizTests : ApplicationContextForTesting
     {
+        private readonly ApplicationDBContext _context;
+
+        public JoinLiveQuizTests(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
         [Fact]
         public async Task JoinRoomTest()
         {
@@ -28,18 +37,19 @@
 
             await client.StartAsync();
 
+            var connectionId = Guid.NewGuid();
+
             // Act
             await client.InvokeAsync("JoinRoom", new UserConnection
             {
-                UserId = new Guid("5b048913-5df0-429f-a42b-051904672e4d"),
-                ConnectionId =  client.ConnectionId         
-                /* set properties here */
+                UserId = Guid.Parse("5b048913-5df0-429f-a42b-051904672e4d"),
+                ConnectionId = connectionId.ToString()
             });
 
             // Assert
-            var userConnection = await _context.UserConnections.FindAsync(client.ConnectionId);
+            var userConnection = await _context.UserConnections.FindAsync(connectionId);
             Assert.NotNull(userConnection); // Verifies that the UserConnection was saved in the database
-            Assert.Equal("5b048913-5df0-429f-a42b-051904672e4d", userConnection.UserId); // Verifies that the UserId is correct
+            Assert.Equal(Guid.Parse("5b048913-5df0-429f-a42b-051904672e4d"), userConnection.UserId); // Verifies that the UserId is correct
         }
-
     }
+}
