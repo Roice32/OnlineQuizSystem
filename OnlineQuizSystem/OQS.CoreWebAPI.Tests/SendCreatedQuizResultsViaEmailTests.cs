5c06b7272aacbeaf6d8741778b3feff2323a25c9
Roice32 <alexandru.butnaru32@yahoo.com>
2024-05-18 22:43:55 +0300

    Manual fetch from TeamIV

---
+++ OnlineQuizSystem/OQS.CoreWebAPI.Tests/SendCreatedQuizResultsViaEmailTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using FluentAssertions;
+using OQS.CoreWebAPI.Tests.SetUp;
+
+
+namespace OQS.CoreWebAPI.Tests
+{
+    public class SendCreatedQuizResultsViaEmailTests : ApplicationContextForTesting
+    {
+        // DISCLAIMER!
+        // Tests don't pass on some machines based on date format.
+
+        [Fact]
+        public async Task Given_ValidParameters_When_SendCreatedQuizStatsViaEmailIsCalled_Then_StatusCodeOkIsReturned()
+        {
+            // Arrange
+            var recipientEmail = "alexandru.butnaru32@yahoo.com";
+            var quizId = Guid.Parse("00000000-0000-0000-0002-000000000001");
+            var startDate = DateTime.UtcNow.AddDays(-7);
+            var endDate = DateTime.UtcNow;
+            var requestUri = $"api/email/sendCreatedQuizStatsViaEmail?" +
+                $"quizId={quizId}&" +
+                $"recipientEmail={recipientEmail}&" +
+                $"startDate={startDate}&" +
+                $"endDate={endDate}";
+
+            // Act
+            var result = await Client.GetAsync(requestUri);
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ OnlineQuizSystem/OQS.CoreWebAPI/OQS.CoreWebAPI/Shared/ResultT.cs
@@ -0,0 +1,13 @@
+﻿namespace OQS.CoreWebAPI.Shared
+{
+    public class Result<T> : Result
+    {
+        private readonly T? _value;
+        protected internal Result(T? value, bool isSuccess, Error error) : base(isSuccess, error)
+        => _value = value;
+
+
+        public T Value => IsSuccess ? _value! : throw new InvalidOperationException();
+        public static implicit operator Result<T>(T? value) => Create(value);
+    }
+}
