bfcfaf60996429e24b213fe33fb101a3a1e181e5
denisagolache <126023804+denisagolache@users.noreply.github.com>
2024-05-19 14:15:12 +0200

    updated

---
+++ OnlineQuizSystem/OQS.CoreWebAPI/OQS.CoreWebAPI/ResultsAndStatisticsModule/Temp/Tag.cs
@@ -0,0 +1,8 @@
+﻿namespace OQS.CoreWebAPI.ResultsAndStatisticsModule.Temp;
+
+public class Tag
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedOnUtc { get; set; }
+}
