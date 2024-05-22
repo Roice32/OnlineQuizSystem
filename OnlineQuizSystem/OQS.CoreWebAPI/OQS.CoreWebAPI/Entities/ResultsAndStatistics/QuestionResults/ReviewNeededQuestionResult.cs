232a98fc91132fadf70ead06f18ff13f0717b8dc
emiliaaa25 <galatanuemilia14@gmail.com>
2024-05-22 16:03:16 +0200

    luat de pe branch

---
+++ OnlineQuizSystem/OQS.CoreWebAPI/OQS.CoreWebAPI/Entities/ResultsAndStatistics/QuestionResults/ReviewNeededQuestionResult.cs
@@ -0,0 +1,18 @@
+﻿using OQS.CoreWebAPI.Entities.ResultsAndStatistics;
+
+namespace OQS.CoreWebAPI.Entities.ResultsAndStatistics.QuestionResults
+{
+    public class ReviewNeededQuestionResult : QuestionResultBase
+    {
+        public string ReviewNeededAnswer { get; set; }
+        public string LLMReview { get; set; } = string.Empty;
+        public AnswerResult ReviewNeededResult { get; set; }
+
+        public ReviewNeededQuestionResult(Guid userId, Guid questionId, float score, string reviewNeededAnswer, AnswerResult reviewNeededResult) :
+            base(userId, questionId, score)
+        {
+            ReviewNeededAnswer = reviewNeededAnswer;
+            ReviewNeededResult = reviewNeededResult;
+        }
+    }
+}
