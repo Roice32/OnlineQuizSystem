fc13de33855688ba7f48ad37da7161ce8244fb5a
emiliaaa25 <galatanuemilia14@gmail.com>
2024-05-26 23:27:27 +0200

    luat de pe team

---
+++ OnlineQuizSystem/OQS.CoreWebAPI/OQS.CoreWebAPI/Contracts/ResultsAndStatistics/FetchQuizResultBodyResponse.cs
@@ -0,0 +1,11 @@
+﻿using OQS.CoreWebAPI.Entities.ResultsAndStatistics.QuestionResults;
+using OQS.CoreWebAPI.Temp;
+
+namespace OQS.CoreWebAPI.Contracts.ResultsAndStatistics
+{
+    public class FetchQuizResultBodyResponse
+    {
+        public List<QuestionBase> Questions { get; set; } = new();
+        public List<QuestionResultBase> QuestionResults { get; set; } = new();
+    }
+}
