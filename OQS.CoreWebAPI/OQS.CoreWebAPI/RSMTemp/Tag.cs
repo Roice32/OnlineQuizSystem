5241f7e19f054f1dd770fff7ceb51f4be8c92038
emiliaaa25 <galatanuemilia14@gmail.com>
2024-05-27 21:35:30 +0200

    .

--- OQS.CoreWebAPI/OQS.CoreWebAPI/RSMTemp/Tag.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OQS.CoreWebAPI.Temp;
-
-public class Tag
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public DateTime CreatedOnUtc { get; set; }
-}
--- OQS.CoreWebAPI/OQS.UserInterface/results-visualization/src/Router/Routes/SubmittedQuizRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { RouteObject } from "react-router-dom";
-import SubmittedQuiz from "../../Pages/SubmittedQuiz";
-import ErrorPage from "../../Pages/ErrorPage";
-
-export const SubmittedQuizRoute: RouteObject = {
-  path: "submittedQuiz",
-  element: <SubmittedQuiz />,
-  errorElement: <ErrorPage />,
-};
