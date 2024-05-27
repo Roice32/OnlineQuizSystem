4bb14388640db90b6a586a3e0e96cf271a658d20
Roice32 <alexandru.butnaru32@yahoo.com>
2024-05-27 21:16:23 +0300

    Fetched Frontend

---
+++ OQS.WebUI/results-and-statistics-module/src/Components/SubmittedQuestions/WrittenQuestionResultDisplay.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { QuestionBase } from "../../utils/types/questions";
+import { Answer } from "../../utils/types/active-quiz";
+
+interface WrittenQuestionResultDisplayProps {
+  question: QuestionBase;
+  userAnswer: Answer;
+  correctAnswer: string;
+  questionText: string;
+  questionScore: number;
+}
+
+export default function WrittenQuestionResultDisplay({userAnswer, correctAnswer, questionText, questionScore }: WrittenQuestionResultDisplayProps) {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-full max-w-4xl bg-[#6a8e8f] rounded-lg">
+        <h2 className="text-2xl font-bold mb-4 text-center">{questionText}</h2>
+        <h3 className="text-1xl mb-4 text-center">Score: {questionScore} points</h3>
+        <div className="flex items-center">
+          <div className="w-1/3 p-4">
+            <img src="/src/utils/mocks/question-mark.png" alt="Question" className="w-full h-auto rounded-lg shadow-2xl" />
+          </div>
+          <div className="w-2/3 p-4 bg-[#6a8e8f] rounded-lg">
+            <div className="space-y-4">
+              <div className="flex items-center justify-center mb-2">
+                <label
+                  className='p-2 rounded-full border border-gray-300 w-full max-w-md text-left bg-green-500 text-white'
+                >
+                  Your Answer: {userAnswer.writeAnswer?.toString()}
+                </label>
+              </div>
+              <div className="flex items-center justify-center mb-2">
+                <label
+                  className='p-2 rounded-full border border-gray-300 w-full max-w-md text-left bg-blue-500 text-white'
+                >
+                  Correct Answer: {correctAnswer}
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ OQS.WebUI/results-and-statistics-module/src/utils/types/results-and-statistics/quiz-results.ts
@@ -0,0 +1,58 @@
+import { QuestionType } from "../questions";
+import { AnswerResult } from "./question-review";
+
+export type QuizResultHeader = {
+    quizId: string;
+    userId: string;
+    userName: string;
+    quizName: string;
+    completionTime: string;
+    score: number;
+    reviewPending: boolean;
+  }
+  
+  export type Question = {
+    id: string;
+    quizId: string;
+    type: QuestionType;
+    text: string;
+    allocatedPoints: number;
+
+    choices?: string[];
+    trueFalseAnswer?: boolean;
+    multipleChoiceAnswer?: string[];
+    singleChoiceAnswer?: string;
+    writtenAcceptedAnswers?: string[];
+  }
+  
+  export type QuestionResult = {
+    userId: string;
+    questionId: string;
+    score: number;
+    type: QuestionType;
+
+    trueFalseAnswerResult?: AnswerResult;
+
+    pseudoDictionaryChoicesResults?: string;
+
+    writtenAnswer?: string;
+    writtenAnswerResult?: AnswerResult;
+
+    reviewNeededAnswer?: string;
+    LLMReview?: string;
+    reviewNeededResult?: AnswerResult;
+  }
+  
+  export type QuizResultBody = {
+    questions: Question[];
+    questionResults: QuestionResult[];
+  }
+  
+  export type QuizResults =  {
+    then(arg0: () => void): unknown;
+    userId : string;
+    quizId : string;
+    quizResultHeader: QuizResultHeader;
+    quizResultBody: QuizResultBody;
+  }
+  
