9783cc2f073fd75978d1ccd43e37e079e2ec3685
PopoiuAndra <126085931+PopoiuAndra@users.noreply.github.com>
2024-05-29 12:53:44 +0300

    add

---
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/Entities/ResultsAndStatistics/CustomJsonDeserializer.cs
@@ -0,0 +1,122 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using OQS.CoreWebAPI.Entities.ResultsAndStatistics.QuestionAnswerPairs;
+using OQS.CoreWebAPI.Entities.ResultsAndStatistics.QuestionResults;
+using OQS.CoreWebAPI.Temp;
+using OQS.CoreWebAPI.Contracts;
+using OQS.CoreWebAPI.Contracts.ResultsAndStatistics;
+
+namespace OQS.CoreWebAPI.Entities.ResultsAndStatistics
+{
+    public class CustomJsonDeserializer : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(QuestionBase) ||
+                objectType == typeof(QuestionResponse) ||
+                objectType == typeof(QuestionAnswerPairBase) ||
+                objectType == typeof(QuestionResultBase) ||
+                objectType == typeof(QuestionResultResponse);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jo = JObject.Load(reader);
+            if (objectType == typeof(QuestionAnswerPairBase))
+            {
+                return DeserializeQuestionAnswerPair(jo);
+            }
+            else if (objectType == typeof(QuestionResultBase))
+            {
+                return DeserializeQuestionResult(jo);
+            }
+            else if (objectType == typeof(QuestionResponse))
+            {
+                return DeserializeQuestionResponse(jo);
+            }
+            else if (objectType == typeof(QuestionResultResponse))
+            {
+                return DeserializeQuestionResultResponse(jo);
+            }
+            return DeserializeQuestion(jo);
+        }
+
+        public QuestionBase DeserializeQuestion(JObject jo)
+        {
+            if (jo["TrueFalseAnswer"] != null)
+            {
+                return jo.ToObject<TrueFalseQuestion>();
+            }
+            else if (jo["SingleChoiceAnswer"] != null)
+            {
+                return jo.ToObject<SingleChoiceQuestion>();
+            }
+            else if (jo["MultipleChoiceAnswers"] != null)
+            {
+                return jo.ToObject<MultipleChoiceQuestion>();
+            }
+            else if (jo["WrittenAcceptedAnswers"] != null)
+            {
+                return jo.ToObject<WrittenAnswerQuestion>();
+            }
+            return jo.ToObject<ReviewNeededQuestion>();
+        }
+
+        public QuestionResponse DeserializeQuestionResponse(JObject jo)
+        {
+            return jo.ToObject<QuestionResponse>();
+        }
+
+        public QuestionAnswerPairBase DeserializeQuestionAnswerPair(JObject jo)
+        {
+            if (jo["TrueFalseAnswer"] != null)
+            {
+                return jo.ToObject<TrueFalseQAPair>();
+            }
+            else if (jo["SingleChoiceAnswer"] != null)
+            {
+                return jo.ToObject<SingleChoiceQAPair>();
+            }
+            else if (jo["MultipleChoiceAnswers"] != null)
+            {
+                return jo.ToObject<MultipleChoiceQAPair>();
+            }
+            else if (jo["WrittenAnswer"] != null)
+            {
+                return jo.ToObject<WrittenQAPair>();
+            }
+            return null;
+        }
+
+        public QuestionResultResponse DeserializeQuestionResultResponse(JObject jo)
+        {
+            return jo.ToObject<QuestionResultResponse>();
+        }
+
+        public QuestionResultBase DeserializeQuestionResult(JObject jo)
+        {
+            if (jo["TrueFalseAnswerResult"] != null)
+            {
+                return jo.ToObject<TrueFalseQuestionResult>();
+            }
+            else if (jo["PseudoDictionaryChoicesResults"] != null)
+            {
+                return jo.ToObject<ChoiceQuestionResult>();
+            }
+            else if (jo["WrittenAnswerResult"] != null)
+            {
+                return jo.ToObject<WrittenAnswerQuestionResult>();
+            }
+            else if (jo["ReviewNeededResult"] != null)
+            {
+                return jo.ToObject<ReviewNeededQuestionResult>();
+            }
+            return null;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/Migrations/20240521192921_Final-RSM-Database.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OQS.CoreWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalRSMDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QuestionResults",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuestionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Score = table.Column<float>(type: "real", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(34)", maxLength: 34, nullable: false),
+                    PseudoDictionaryChoicesResults = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReviewNeededAnswer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LLMReview = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReviewNeededResult = table.Column<int>(type: "int", nullable: true),
+                    TrueFalseAnswerResult = table.Column<int>(type: "int", nullable: true),
+                    WrittenAnswer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WrittenAnswerResult = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionResults", x => new { x.UserId, x.QuestionId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AllocatedPoints = table.Column<int>(type: "int", nullable: false),
+                    QuizId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(34)", maxLength: 34, nullable: false),
+                    Choices = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MultipleChoiceAnswers = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SingleChoiceAnswer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TrueFalseAnswer = table.Column<bool>(type: "bit", nullable: true),
+                    WrittenAcceptedAnswers = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuizResultHeaders",
+                columns: table => new
+                {
+                    QuizId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SubmittedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CompletionTime = table.Column<int>(type: "int", nullable: false),
+                    Score = table.Column<float>(type: "real", nullable: false),
+                    ReviewPending = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuizResultHeaders", x => new { x.UserId, x.QuizId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Quizzes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TimeLimitMinutes = table.Column<int>(type: "int", nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quizzes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOnUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuestionResults");
+
+            migrationBuilder.DropTable(
+                name: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "QuizResultHeaders");
+
+            migrationBuilder.DropTable(
+                name: "Quizzes");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
