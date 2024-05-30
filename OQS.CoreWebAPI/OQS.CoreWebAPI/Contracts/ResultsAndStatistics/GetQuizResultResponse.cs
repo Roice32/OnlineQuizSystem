1867e4e34e407eb8084d6d58876fcd295891b654
emiliaaa25 <galatanuemilia14@gmail.com>
2024-05-30 19:16:21 +0200

    .

--- OQS.CoreWebAPI/OQS.CoreWebAPI/Contracts/ResultsAndStatistics/GetQuizResultResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OQS.CoreWebAPI.Contracts.ResultsAndStatistics
-{
-    public class GetQuizResultResponse
-    {
-        public FetchQuizResultHeaderResponse QuizResultHeader { get; set; }
-        public FetchQuizResultBodyResponse QuizResultBody { get; set; }
-    }
-}
