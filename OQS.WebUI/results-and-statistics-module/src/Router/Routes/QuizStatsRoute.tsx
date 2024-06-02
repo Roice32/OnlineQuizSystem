ad2b6eca863895c608b2d8b0f5ff30ae2569cba2
denisagolache <126023804+denisagolache@users.noreply.github.com>
2024-06-02 05:36:04 +0200

    deleted

--- OQS.WebUI/results-and-statistics-module/src/Router/Routes/QuizStatsRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { RouteObject } from "react-router-dom";
-import ErrorPage from "../../Pages/ErrorPage";
-import QuizStatsPage from "../../Pages/CreatedQuizStatsPage";
-
-export const QuizStatsRoute: RouteObject = {
-  path: "quizStats/:quizId",
-  element: <QuizStatsPage />,
-  errorElement: <ErrorPage />,
-};
--- OQS.WebUI/results-and-statistics-module/src/utils/types/result.ts
+++
@@ -1,8 +0,0 @@
-import { Error } from "./error"
-
-export type Result<T> = {
-    value?:T,
-    error?:Error,
-    isSuccess:boolean,
-    isFailure:boolean
-}
