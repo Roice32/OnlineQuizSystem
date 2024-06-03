d966833400e946f9020a09f26e8c980815a1b686
iulica04 <ivreme12@gmail.com>
2024-06-03 23:12:24 +0300

    ..

--- OQS.CoreWebAPI/OQS.CoreWebAPI/Features/Authentication/Authentication.cs
+++ OQS.CoreWebAPI/OQS.CoreWebAPI/Features/Authentication/Authentication.cs
@@ -90,7 +90,7 @@ namespace OQS.CoreWebAPI.Features.Authentication
                 {
                     Issuer = configuration["JWT:ValidIssuer"]!,
                     Audience = configuration["JWT:ValidAudience"]!,
-                    Expires = DateTime.UtcNow.AddHours(24),
+                    Expires = DateTime.UtcNow.AddDays(7),
                     SigningCredentials = new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256),
                     Subject = new ClaimsIdentity(claims)
                 };
