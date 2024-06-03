b3e7d098d679e38acfbea32c2f6375685edea73f
Alexandru Frunza <alex@alexfrunza.com>
2024-06-04 02:01:34 +0300

    Add edit quiz page

--- OQS.WebUI/src/Router/Routes/QuizRoute.tsx
+++ OQS.WebUI/src/Router/Routes/QuizRoute.tsx
@@ -1,31 +1,37 @@
-import { Outlet, RouteObject } from "react-router-dom";
-import QuizPage, { QuizzesLoader } from "../../Pages/QuizPage";
+import {Outlet, RouteObject} from "react-router-dom";
+import QuizPage, {QuizzesLoader} from "../../Pages/QuizPage";
 import QuizDetailsPage, {
-  QuizDetailsLoader,
+    QuizDetailsLoader,
 } from "../../Pages/QuizDetailsPage";
 import ErrorPage from "../../Pages/ErrorPage";
 import QuizCreate from "../../Pages/QuizCreate/QuizCreate.tsx";
+import EditQuizPage from "../../Pages/QuizEdit/EditQuizPage.tsx";
 
 export const QuizRoute: RouteObject = {
-  path: "quizzes",
-  element: <Outlet />,
-  children: [
-    {
-      path: "",
-      loader: QuizzesLoader,
-      errorElement: <ErrorPage />,
-      element: <QuizPage />,
-    },
-    {
-      path: ":id",
-      loader: QuizDetailsLoader,
-      errorElement: <ErrorPage />,
-      element: <QuizDetailsPage />,
-    },
-    {
-      path: "create",
-      errorElement: <ErrorPage />,
-      element: <QuizCreate />,
-    },
-  ],
+    path: "quizzes",
+    element: <Outlet/>,
+    children: [
+        {
+            path: "",
+            loader: QuizzesLoader,
+            errorElement: <ErrorPage/>,
+            element: <QuizPage/>,
+        },
+        {
+            path: ":id",
+            loader: QuizDetailsLoader,
+            errorElement: <ErrorPage/>,
+            element: <QuizDetailsPage/>,
+        },
+        {
+            path: "create",
+            errorElement: <ErrorPage/>,
+            element: <QuizCreate/>,
+        },
+        {
+            path: "update/:id",
+            errorElement: <ErrorPage/>,
+            element: <EditQuizPage/>,
+        },
+    ],
 };
