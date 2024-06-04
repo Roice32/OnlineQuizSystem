1fe52a0494029c6484b4fba5e7e980e2804be5c6
Zebega George-Lucian <91672147+ZebegaGeorgeLucian@users.noreply.github.com>
2024-06-04 16:09:04 +0300

    homepage

--- OQS.WebUI/src/Pages/HomePage.tsx
+++ OQS.WebUI/src/Pages/HomePage.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#1c4e4f] p-6">
       <div className="text-center mb-8 animate-bounce">
-        <h1 className="text-6xl font-bold animate-pulseColor mb-4">
+        <h1 className="text-6xl font-bold animate-blingBling mb-4">
           Welcome to B4Quiz App
         </h1>
       </div>
