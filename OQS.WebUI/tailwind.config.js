6d2ad362ec85c873541e5408668b89ef8f0e89cb
Zebega George-Lucian <91672147+ZebegaGeorgeLucian@users.noreply.github.com>
2024-06-04 14:09:24 +0300

    modify HomePage

--- OQS.WebUI/tailwind.config.js
+++ OQS.WebUI/tailwind.config.js
@@ -5,8 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        pulseColor: {
+          '0%, 100%': { color: '#1c4e4f' },
+          '50%': { color: '#3d7a7b' },
+        }
+      },
+      animation: {
+        pulseColor: 'pulseColor 2s cubic-bezier(0, 0, 0.2, 1) infinite',
+      }
+    }
   },
   plugins: [],
 }
-
